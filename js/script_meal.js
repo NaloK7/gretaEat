4aad360455319ca6c4cbfa3c13f286822b8efd54
NaloK <129076162+NaloK7@users.noreply.github.com>
2024-03-08 22:06:18 +0100

    add recipe display in meal.html

--- js/script_meal.js
+++ js/script_meal.js
@@ -34,34 +34,43 @@ async function fullRecipe() {
   let country = gmr.meals[0].strArea;
   document.querySelector("#country").innerHTML = country;
 
-  console.log(gmr.meals);
 
-  // todo boucles sur la longueur des cle pour pouvoir associer valeur x et valeur y en une seule boucle
   // ingredients + quantity
   let ingredientsName = [];
   let ingredientsQuantity = [];
-  let ingredientsAndQuantity = {};
 
+  // iterate over the keys and values
   for (const [key, value] of Object.entries(gmr.meals[0])) {
+    //   if value is not null or empty
     if (value != "" && value != " " && value != null) {
+      // if the key include the word 'ingredient'
       if (key.includes(`Ingredient`)) {
+        //   push the value: name of the ingredient in the array ingredientsName
         ingredientsName.push(value);
+        //   same as above to get the quantity
       } else if (key.includes(`Measure`)) {
         ingredientsQuantity.push(value);
       }
     }
   }
-  // alert(gmr.meals[0].strIngredient)
 
-  // display ingredients + quantity
+  // select a div where insert ingredient details
   let displayIng = document.querySelector("#ingredients");
-
+  // associate the name and the quantity of an ingredient and display it
   for (let i = 0; i < ingredientsName.length; i++) {
     displayIng.innerHTML += `<p>${ingredientsName[i]}: ${ingredientsQuantity[i]}</p>`;
   }
 
+
   // instructions
+  // todo: maybe find a pattern in recipe to split them in multiple line
   let recipe = gmr.meals[0].strInstructions;
+  document.querySelector('#recipe').innerHTML += `<p>${recipe}</p>`
+  // let steps = recipe.split("STEP ")
+  // for (let i = 1; i < steps.length; i++) {
+  //   document.querySelector('#recipe').innerHTML += `<p>STEP ${steps[i]}</p>`
+  // }
+
 }
 
 fullRecipe();
