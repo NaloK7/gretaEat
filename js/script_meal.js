311da52e361c4bc57ea42552d359e335a32db563
NaloK <129076162+NaloK7@users.noreply.github.com>
2024-03-08 09:37:25 +0100

    v0.0.1

--- js/script_meal.js
+++ js/script_meal.js
@@ -2,67 +2,66 @@
 // recupere l'id d'un plat
 // requete url avec id pour recup info
 
-
 async function fullRecipe() {
+  let url = document.location;
 
-    let url = document.location
-   
-    let search = url.search
+  let search = url.search;
 
-    let param = new URLSearchParams(search);
-    let id = parseInt(param.get("id"))
+  let param = new URLSearchParams(search);
+  let id = parseInt(param.get("id"));
 
-    // get meal recipe
-    let gmr = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-    gmr = await gmr.json()
+  // get meal recipe
+  let gmr = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+  gmr = await gmr.json();
 
-    // nom
-    let name = gmr.meals[0].strMeal
-    document.querySelector('#this-name').innerHTML = name
-    document.querySelector('title').innerHTML += name
+  // nom
+  let name = gmr.meals[0].strMeal;
+  document.querySelector("#this-name").innerHTML = name;
+  document.querySelector("title").innerHTML += name;
 
-    // img path
-    let thumbs = gmr.meals[0].strMealThumb
-    document.querySelector('#this-thumbs').src = thumbs
-    document.querySelector('#this-thumbs').alt = name
-    
-    // type
-    let category = gmr.meals[0].strCategory
-    document.querySelector('#categorie').innerHTML = category
+  // img path
+  let thumbs = gmr.meals[0].strMealThumb;
+  document.querySelector("#this-thumbs").src = thumbs;
+  document.querySelector("#this-thumbs").alt = name;
 
-    // origine
-    let country = gmr.meals[0].strArea
-    document.querySelector('#country').innerHTML = country
+  // type
+  let category = gmr.meals[0].strCategory;
+  document.querySelector("#categorie").innerHTML = category;
 
-    console.log(gmr.meals)
-    
-    // ingredients + quantity    
-    let ingredientsName = []
-    let ingredientsQuantity = []
-    let ingredientsAndQuantity = {}
+  // origine
+  let country = gmr.meals[0].strArea;
+  document.querySelector("#country").innerHTML = country;
 
-    for (const [key, value] of Object.entries(gmr.meals[0])) {
-        if (value != "" && value != " " && value != null) {
-            if (key.includes(`Ingredient`)) {
-                ingredientsName.push(value)
-            } else if (key.includes(`Measure`)) {
-                ingredientsQuantity.push(value)                
-            }
-        }
+  console.log(gmr.meals);
+
+  // todo boucles sur la longueur des cle pour pouvoir associer valeur x et valeur y en une seule boucle
+  // ingredients + quantity
+  let ingredientsName = [];
+  let ingredientsQuantity = [];
+  let ingredientsAndQuantity = {};
+
+  for (const [key, value] of Object.entries(gmr.meals[0])) {
+    if (value != "" && value != " " && value != null) {
+      if (key.includes(`Ingredient`)) {
+        ingredientsName.push(value);
+      } else if (key.includes(`Measure`)) {
+        ingredientsQuantity.push(value);
+      }
     }
-    // alert(gmr.meals[0].strIngredient)
+  }
+  // alert(gmr.meals[0].strIngredient)
 
-    
-    // display ingredients + quantity
-    let displayIng = document.querySelector('#ingredients')
+  // display ingredients + quantity
+  let displayIng = document.querySelector("#ingredients");
 
-    for (let i = 0; i < ingredientsName.length; i++) {
-        displayIng.innerHTML += `<p>${ingredientsName[i]}: ${ingredientsQuantity[i]}</p>`;
-        
-    }
+  for (let i = 0; i < ingredientsName.length; i++) {
+    displayIng.innerHTML += `<p>${ingredientsName[i]}: ${ingredientsQuantity[i]}</p>`;
+  }
 
-    // instructions
-    let recipe = gmr.meals[0].strInstructions
+  // instructions
+  let recipe = gmr.meals[0].strInstructions;
 }
 
-fullRecipe()
+fullRecipe();
