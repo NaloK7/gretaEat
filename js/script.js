9c45b875e800ad735b7026729feb808e9b8f17bc
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-03-12 17:29:58 +0100

    trying to track current page in script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 
+let path = document.location
+console.log("🚀 ~ path:", path)
 
+let previous = document.referrer
+console.log("🚀 ~ previous:", previous)
+
+
+
+let logoPaths = [
+    "/img/logo1.png",
+    "/img/logo2.png",
+    "/img/logo3.png",
+    "/img/logo4.png",
+    "/img/logo5.png",
+    "/img/logo6.png"
+]
+
+if (previous != path) {
+    let src = document.querySelector('#logoimg').src
+    src = src.split("/")
+    console.log("🚀 ~ src:", src)
+    src.slice(-1)
+
+    console.log("🚀 ~ src:", src)
+
+} else {
+    console.log('pas changer !!')
+}
