fabd530ba74cba2a748b6d6c77b083f08216285d
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-03-12 09:59:02 +0100

    transfert import of font from html to css

--- js/script_first_letter.js
+++ js/script_first_letter.js
@@ -3,8 +3,12 @@
 let alphabet = []
 
 for (let i = 0; i < 26; i++) {
+    if (i != 0) {
+        document.querySelector(".alphabet").innerHTML += '<div> / </div>'
+    }
     let letter = String.fromCharCode(65+i);
-    document.querySelector(".test").innerHTML += `<a href="https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}">${letter}</a>`  
+    document.querySelector(".alphabet").innerHTML += `<a href="https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}">${letter}</a>`
+
 }
 // 
 // `https://www.themealdb.com/api/json/v1/1/search.php?f=`
