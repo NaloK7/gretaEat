1fba6d31144a6fc2d94e345d1ed9e0060616f630
NaloK <129076162+NaloK7@users.noreply.github.com>
2024-03-14 16:49:22 +0100

    move header from html to script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
+document.querySelector(
+  "header"
+).innerHTML = `
+<a id="logo" href="index.html"><img id="logoimg" src="img/logo2.png" alt="logo greta eat">
+<h1 id="logotxt">GretaEat</h1>
+</a>
+<nav>
+<a href="random.html"><i class="fas fa-random"></i></a>
+<a id="all-cat-btn" href="categories.html">Categories</a>
+<a href="first-letter.html">A - Z</a>
+<a href="search.html"><i class="fas fa-search"></i> Rechercher</a>
+</nav>`;
 
 let logoPaths = [
-    "img/logo1.png",
-    "img/logo2.png",
-    "img/logo3.png",
-    "img/logo4.png",
-    "img/logo5.png",
-    "img/logo6.png"
-]
+  "img/logo1.png",
+  "img/logo2.png",
+  "img/logo3.png",
+  "img/logo4.png",
+  "img/logo5.png",
+  "img/logo6.png",
+];
 
 function incrementCounter() {
-    let counter = localStorage.getItem('counter');
-    counter = counter ? parseInt(counter) + 1 : 1;
-    localStorage.setItem('counter', counter);
-    let logo = document.querySelector('#logo')
-    logo.innerHTML = `<img id="logoimg" src="img/logo${(counter%(logoPaths.length-1))+1}.png" alt="logo greta eat">
-    <h1 id="logotxt">GretaEat</h1>`
-    return counter;
+  let counter = localStorage.getItem("counter");
+  counter = counter ? parseInt(counter) + 1 : 1;
+  localStorage.setItem("counter", counter);
+  let logo = document.querySelector("#logo");
+  logo.innerHTML = `<img id="logoimg" src="img/logo${
+    (counter % (logoPaths.length - 1)) + 1
+  }.png" alt="logo greta eat">
+    <h1 id="logotxt">GretaEat</h1>`;
+  return counter;
 }
 
 incrementCounter();
+
